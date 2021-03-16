e69175528aa6f635fbc65b8bc0f008a6f9038a8f
Nachiket Trivedi <nachikettrivedi@Nachikets-MacBook-Air.local>
2021-03-15 23:13:13 -0700

    Stage 1: cvs file upload on browser

---
+++ mobileFrontend/ios/Pods/Target Support Files/Pods-mobilefrontend/Pods-mobilefrontend-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mobilefrontend : NSObject
+@end
+@implementation PodsDummy_Pods_mobilefrontend
+@end
--- webfrontend/src/Containers/ECGAnalysisPage/ECGAnalysis.js
+++ webfrontend/src/Containers/ECGAnalysisPage/ECGAnalysis.js
@@ -5,38 +5,31 @@ import styles from "../../Styles/styles.module.css";
 import UploadECG from "../../Components/ECGAnalysis/UploadECG";
 import ECGReports from "../../Components/ECGAnalysis/ECGReports";
 
-class ECGAnalysis extends React.Component {
-  constructor(props) {
-    super(props);
+const ECGAnalysis = () => {
+  if (localStorage.getItem("email") == null) {
+    return <Redirect to="/" />;
+  }
+  if (localStorage.getItem("role") == "admin") {
+    return <Redirect to="/" />;
   }
 
-  render() {
-    if (localStorage.getItem("email") == null) {
-      return <Redirect to="/" />;
-    }
-
-    if ( localStorage.getItem("role") == "admin") {
-        return <Redirect to="/" />;
-    }
-
-    if (
-      localStorage.getItem("role") == "patient" &&
-      (localStorage.getItem("medicalFlag") == 0 ||
-        localStorage.getItem("medicalFlag") == "false")
-    ) {
-      return <Redirect to="/completeMedicalProfile" />;
-    }
-
-    return (
-      <div className={styles.colorbg}>
-        <br />
-        <UploadECG />
-        <br />
-        <ECGReports />
-        <br />
-      </div>
-    );
+  if (
+    localStorage.getItem("role") == "patient" &&
+    (localStorage.getItem("medicalFlag") == 0 ||
+      localStorage.getItem("medicalFlag") == "false")
+  ) {
+    return <Redirect to="/completeMedicalProfile" />;
   }
-}
+
+  return (
+    <div className={styles.colorbg}>
+      <br />
+      <UploadECG />
+      <br />
+      <ECGReports />
+      <br />
+    </div>
+  );
+};
 
 export default ECGAnalysis;

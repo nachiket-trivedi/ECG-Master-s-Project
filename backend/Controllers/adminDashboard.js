d96d470bb6d2b2bab2fef28feec04e6f69f4eb63
Alaukika Diwanji <alaukikadiwanji@Alaukikas-MacBook-Pro.local>
2021-04-19 23:13:50 -0500

    Dashboard connected to backend - Web - ECG Records Admin

--- backend/Controllers/adminDashboard.js
+++ backend/Controllers/adminDashboard.js
@@ -46,4 +46,71 @@ router.get("/userCount", async function(req, res) {
   }
 });
 
+
+router.get("/ecgRecords", async function(req, res) {
+    console.log("In Get Number of ECG Records");
+
+    let con = await dbConnection();
+    var pkg;
+    let normal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let abnormal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  
+    try {
+      let query = `SELECT class, timestamp FROM abnormal_ecg`;
+      con.query(query, function(err, result_abnormal, fields) {
+        if (err) {
+          console.log("Unable to fetch data");
+          pkg = {
+            errorType: "Error in fetching abnormal"
+          };
+          console.log(pkg);
+          res.status(205).send(JSON.stringify(pkg));
+          res.end("Unable to fetch data!");
+        } else {
+          let query2 = `SELECT timestamp FROM normal_ecg`;
+          con.query(query2, function(err, result_normal, fields) {
+            for (var i = 0; i < result_abnormal.length; i++) {
+              var temp = result_abnormal[i].timestamp.toISOString().split("-");
+              console.log(temp[1]);
+              abnormal[temp[1] - 1]++;
+            }
+            abnormal = abnormal.map(item => {
+              return Math.ceil(item / 3);
+            });
+            if (err) {
+              console.log(err);
+              pkg = {
+                abnormal: abnormal,
+                normal: normal,
+                errorType: "Error in fetching normal"
+              };
+              console.log(pkg);
+              res.status(205).send(JSON.stringify(pkg));
+              res.end("Fetched Data with some error!");
+            } else {
+              console.log(result_normal);
+              for (var i = 0; i < result_normal.length; i++) {
+                var temp = result_normal[i].timestamp.toISOString().split("-");
+                normal[temp[1] - 1]++;
+              }
+              normal = normal.map(item => {
+                return item / 3;
+              });
+              pkg = {
+                abnormal: abnormal,
+                normal: normal,
+                errorType: "Error in fetching normal"
+              };
+              res.status(200).send(JSON.stringify(pkg));
+              res.end("Successfully Fetched");
+            }
+          });
+        }
+      });
+    } catch (ex) {
+      console.log(ex);
+      throw ex;
+    }
+  });
+
 module.exports = router;
--- webfrontend/src/Components/Dashboard/Admin/AdminECGCount.js
+++ webfrontend/src/Components/Dashboard/Admin/AdminECGCount.js
@@ -4,73 +4,117 @@ import { Redirect } from "react-router";
 import styles from "../../../Styles/styles.module.css";
 import { Row, Col } from "react-bootstrap";
 
+import axios from "axios";
+import { backendIp, backendPort } from "../../../config";
+
+const hostAddress = `${backendIp}:${backendPort}`;
+const config = {
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+    "Content-Type": "application/json"
+  }
+};
+
 //https://codesandbox.io/s/react-chartjs-2-line-chart-example-5z3ss?from-embed
 //https://stackoverflow.com/questions/59472883/grouped-bar-chart-with-react-chartjs-2
 class AdminECGCount extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
-            datasets: [
-              {
-                label: 'Normal',
-                backgroundColor: ' rgba(75,192,192,1)',
-                borderColor: ' rgba(75,192,192,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(80,192,192,1)',
-                hoverBorderColor: 'rgba(80,192,192,1)',
-                data: [65, 59, 80, 81, 56, 55, 40,78,65,89,80,78]
-              },
-              {
-                label: 'Abnormal',
-                backgroundColor: 'rgba(245, 121, 121,1)',
-                borderColor: 'rgba(245, 121, 121,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(250, 121, 121,1)',
-                hoverBorderColor: 'rgba(250, 121, 121,1)',
-                data: [20, 29, 30, 41, 16, 15, 20,28,21,34,32,38]
-              }
-            ]
-        }
+      data: "",
+      base_normal: [65, 59, 80, 81, 0, 0, 0, 0, 0, 0, 0, 0],
+      base_abnormal: [20, 29, 30, 41, 0, 0, 0, 0, 0, 0, 0, 0]
     };
   }
 
-  render() {
+  componentDidMount = () => {
+    axios.defaults.withCredentials = true;
+    axios
+      .get(hostAddress + "/adminDashboard/ecgRecords", config)
+      .then(response => {
+        console.log(response);
+        var abnormal = [];
+        var normal = [];
+        for (var i = 0; i < 12; i++) {
+          normal[i] = this.state.base_normal[i] + response.data.normal[i];
+          abnormal[i] = this.state.base_abnormal[i] + response.data.abnormal[i];
+        }
 
-    let options={
+        var data = {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+          ],
+          datasets: [
+            {
+              label: "Normal",
+              backgroundColor: " rgba(75,192,192,1)",
+              borderColor: " rgba(75,192,192,1)",
+              borderWidth: 1,
+              hoverBackgroundColor: "rgba(80,192,192,1)",
+              hoverBorderColor: "rgba(80,192,192,1)",
+              data: normal
+            },
+            {
+              label: "Abnormal",
+              backgroundColor: "rgba(245, 121, 121,1)",
+              borderColor: "rgba(245, 121, 121,1)",
+              borderWidth: 1,
+              hoverBackgroundColor: "rgba(250, 121, 121,1)",
+              hoverBorderColor: "rgba(250, 121, 121,1)",
+              data: abnormal
+            }
+          ]
+        };
+        this.setState({
+          data: data
+        });
+      });
+  };
+
+  render() {
+    let options = {
       responsive: true,
       legend: {
-          display: false,
+        display: false
       },
-      type:'bar'
-  };
+      type: "bar"
+    };
 
     return (
       <div>
-        <br/>
+        <br />
         <Row>
           <Col sm={8}>
-            <h4 className={styles.profileTextHeading}>ECG Reports Per Months</h4>
+            <h4 className={styles.profileTextHeading}>
+              ECG Reports Per Months
+            </h4>
           </Col>
         </Row>
-        <br/>
+        <br />
         <div className={styles.graphLabel}>
-            <Row>
-            <Col sm={1}>
-            </Col>
+          <Row>
+            <Col sm={1}></Col>
             <Col sm={10}>
-            <Bar
+              <Bar
                 data={this.state.data}
                 width={null}
                 height={null}
                 options={options}
-            />
+              />
             </Col>
-            <Col sm={1}>
-            </Col>
-            </Row>
-            
+            <Col sm={1}></Col>
+          </Row>
         </div>
       </div>
     );
